ee4050b2fbb1d79040275bbb5c5afac28c2309e4
Nicole Fafard <nicole.fafard@gmail.com>
2019-11-30 14:01:11 -0700

    added protected endpoints

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,13 @@
-process.env.TZ = 'UTC'
-process.env.NODE_ENV = 'test'
+process.env.TZ = "UTC";
+process.env.NODE_ENV = "test";
 
-require('dotenv').config()
-const { expect } = require('chai')
-const supertest = require('supertest')
+require("dotenv").config();
+process.env.TEST_DB_URL =
+  process.env.TEST_DB_URL ||
+  "postgresql://thingful:1234@localhost/thingful-test";
 
-global.expect = expect
-global.supertest = supertest
+const { expect } = require("chai");
+const supertest = require("supertest");
+
+global.expect = expect;
+global.supertest = supertest;

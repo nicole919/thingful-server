1333e0bc83d234f3427632ba873e505a7dca0e9f
Nicole Fafard <nicole.fafard@gmail.com>
2019-12-02 13:34:58 -0700

    added user registration

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const AuthService = require("./auth-service");
 const authRouter = express.Router();
 const jsonBodyParser = express.json();
 
@@ -12,19 +12,29 @@ authRouter.post("/login", jsonBodyParser, (req, res, next) => {
       return res.status(400).json({
         error: `Missing '${key}' in request body`
       });
-  return AuthService.comparePasswords(loginUser.password, dbUser.password).then(
-    compareMatch => {
-      if (!compareMatch)
+
+  AuthService.getUserWithUserName(req.app.get("db"), loginUser.user_name).then(
+    dbUser => {
+      if (!dbUser)
         return res.status(400).json({
           error: "Incorrect user_name or password"
         });
-      const sub = dbUser.user_name;
-      const payload = { user_id: dbUser.id };
-      res.send({
-        authToken: AuthService.createJwt(sub, payload)
+
+      return AuthService.comparePasswords(
+        loginUser.password,
+        dbUser.password
+      ).then(compareMatch => {
+        if (!compareMatch)
+          return res.status(400).json({
+            error: "Incorrect user_name or password"
+          });
+        const sub = dbUser.user_name;
+        const payload = { user_id: dbUser.id };
+        res.send({
+          authToken: AuthService.createJwt(sub, payload)
+        });
       });
     }
   );
 });
-
 module.exports = authRouter;

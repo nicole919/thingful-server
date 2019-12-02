ec36a2cfec6eab3fbc0c214f0540f738b1cc1062
Nicole Fafard <nicole.fafard@gmail.com>
2019-12-02 11:52:31 -0700

    added secure login feature

---
+++ src/auth/auth-router.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const authRouter = express.Router();
+const jsonBodyParser = express.json();
+
+authRouter.post("/login", jsonBodyParser, (req, res, next) => {
+  const { user_name, password } = req.body;
+  const loginUser = { user_name, password };
+
+  for (const [key, value] of Object.entries(loginUser))
+    if (value == null)
+      return res.status(400).json({
+        error: `Missing '${key}' in request body`
+      });
+  return AuthService.comparePasswords(loginUser.password, dbUser.password).then(
+    compareMatch => {
+      if (!compareMatch)
+        return res.status(400).json({
+          error: "Incorrect user_name or password"
+        });
+      const sub = dbUser.user_name;
+      const payload = { user_id: dbUser.id };
+      res.send({
+        authToken: AuthService.createJwt(sub, payload)
+      });
+    }
+  );
+});
+
+module.exports = authRouter;
